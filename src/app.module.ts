1b3259b28448e78b6ab51a1f3ceddf5d579d3014
Gabriel Pérez <gph393@gmail.com>
2024-04-23 10:35:10 -0400

    Use dotenv instead of @nestjs/config for reading env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-  ],
+  imports: [],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
+import 'dotenv/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('APP_PORT') || 3000);
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
